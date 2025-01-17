8971c077e537e873e838f1c1adfc03727e9ba717
Fatih Küçük <fatihkucuk@live.com>
2025-01-17 15:15:04 +0300

    Bugünlük yeter :D

--- app/src/main/java/com/fatihkucuk/qrandbarcodescanner/MainActivity.kt
+++ app/src/main/java/com/fatihkucuk/qrandbarcodescanner/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.fatihkucuk.qrandbarcodescanner
 
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.button.MaterialButton
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.common.Barcode
@@ -26,6 +34,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var cameraPermissions: Array<String>
     private lateinit var storagePermissions: Array<String>
+    private var imageUri: Uri? = null
 
     val options = BarcodeScannerOptions.Builder()
         .setBarcodeFormats(
@@ -33,13 +42,10 @@ class MainActivity : AppCompatActivity() {
         .build()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        cameraPermissions = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        storagePermissions = arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
         cameraBtn = findViewById(R.id.cameraBtn)
         galleryBtn = findViewById(R.id.galleryBtn)
         imageTv = findViewById(R.id.imageIv)
@@ -47,15 +53,119 @@ class MainActivity : AppCompatActivity() {
         resultTv = findViewById(R.id.resultTv)
 
         cameraBtn.setOnClickListener{
-
+            if(checkCameraPermission()){
+                pickImageCamera()
+            }
+            else {
+                requestCameraPermission()
+            }
         }
 
         galleryBtn.setOnClickListener{
-
+            if(checkStoragePermission()){
+                pickImageGallery()
+            }
+            else {
+                requestStoragePermission()
+            }
         }
 
         scanBtn.setOnClickListener{
 
         }
+    }
+    private fun pickImageGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        galleryActivityResultLauncher.launch(intent)
+    }
+
+    private val galleryActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            val data = result.data
+            imageUri = data?.data
+            Log.d(TAG, "galleryActivityResultLauncher: imageUri: $imageUri")
+            imageTv.setImageURI(imageUri)
+        }
+        else {
+            showToast("Canceled!")
+        }
+    }
+    private fun pickImageCamera(){
+        val contentValues = ContentValues()
+        contentValues.put(MediaStore.Images.Media.TITLE, "Sample Image")
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Sample Image Description")
+        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        cameraActivityResultLauncher.launch(intent)
+    }
+    private val cameraActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){ result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            val data = result.data
+            Log.d(TAG, "cameraActivityResultLauncher: imageUri: $imageUri")
+            imageTv.setImageURI(imageUri)
+        }
+        else {
+            showToast("Canceled!")
+        }
+    }
+    private fun checkStoragePermission(): Boolean{
+        val result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        return result
+    }
+    private fun requestStoragePermission(){
+        ActivityCompat.requestPermissions(this, storagePermissions, STORAGE_REQUEST_CODE)
+    }
+    private fun checkCameraPermission(): Boolean{
+        val resultCamera = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        val resultStorage = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        return resultCamera && resultStorage
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String?>,
+        grantResults: IntArray,
+        deviceId: Int
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId)
+        when(requestCode){
+            CAMERA_REQUEST_CODE->{
+                if(grantResults.isNotEmpty()){
+                    val cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    val storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED
+                    if(cameraAccepted && storageAccepted){
+                        pickImageCamera()
+                    }
+                    else {
+                        showToast("Camera & Storage permissions are required")
+                    }
+                }
+            }
+
+            STORAGE_REQUEST_CODE->{
+                if(grantResults.isNotEmpty()){
+                    val storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    if(storageAccepted){
+                        pickImageGallery()
+                    }
+                    else {
+                        showToast("Storage permission is required")
+                    }
+                }
+            }
+        }
+    }
+    private fun requestCameraPermission(){
+        ActivityCompat.requestPermissions(this, cameraPermissions, CAMERA_REQUEST_CODE)
+    }
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
